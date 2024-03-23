c26d2e4601f91fff633e8cd87fc31fd63ddf214e
EvelinVillamil5 <e.villamilg@uniandes.edu.co>
2024-03-23 03:34:47 -0500

    Feature 4

--- ventura_front/lib/services/view_models/locations_viewmodel.dart
+++ ventura_front/lib/services/view_models/locations_viewmodel.dart
@@ -1,8 +1,15 @@
+import 'dart:convert';
+
+import 'package:ventura_front/services/models/user_model.dart';
+
 import '../../mvvm_components/viewmodel.dart';
 import '../../mvvm_components/observer.dart';
 
 import '../repositories/locations_repository.dart';
 import '../models/location_model.dart';
+import 'package:ventura_front/services/repositories/user_repository.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+
 
 class LocationsViewModel extends EventViewModel {
 
@@ -11,6 +18,28 @@ class LocationsViewModel extends EventViewModel {
 
   final LocationRepository _repository;
   LocationsViewModel(this._repository);
+
+  void updateUbicarButtonClicks(LocationModel location) {
+    Map<String, dynamic> jsonData = {
+      "location": {
+        "name": location.name,
+        "latitude": location.latitude,
+        "longitude": location.longitude
+      },
+      "clicks": location
+    };
+
+    String jsonString = json.encode(jsonData);
+    String fileName = "ubicar_clicks.json";
+
+    firebase_storage.Reference ref = firebase_storage.FirebaseStorage.instance.ref().child(fileName);
+
+    ref.putString(jsonString).then((firebase_storage.TaskSnapshot storageTask) {
+      print("Archivo JSON subido exitosamente a Firebase Storage.");
+    }).catchError((error) {
+      print("Error al subir el archivo JSON a Firebase Storage: $error");
+    });
+  }
 
   void loadLocations() {
     notify(LoadingEvent(isLoading: true));
