e6a0b0ee3554c8ff3a8d47ef88dae0be42f5c187
Santfogu <79651297+Santfogu@users.noreply.github.com>
2024-04-22 21:43:44 -0500

    MVVM to weather and alert

--- ventura_front/lib/screens/home/view.dart
+++ ventura_front/lib/screens/home/view.dart
@@ -11,27 +11,26 @@ import './components/options_component.dart';
 import '../../services/models/user_model.dart';
 import '../../services/models/weather_model.dart';
 
-class HomeView extends StatefulWidget {
-  const HomeView({super.key});
-
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
 
   @override
-  State<HomeView> createState() => HomeViewState();
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => WeatherViewModel(),
+      child: HomeViewContent(),
+    );
+  }
 }
 
-class HomeViewState extends State<HomeView> {
+class HomeViewContent extends StatefulWidget {
+  @override
+  State<HomeViewContent> createState() => HomeViewContentState();
+}
+
+class HomeViewContentState extends State<HomeViewContent> {
   Position? position;
-
-  WeatherViewModel weatherViewModel = WeatherViewModel();
-
-  WeatherModel weatherData = WeatherModel(
-    description : "Default",
-    location : "Default",
-    temperature : 0,
-    feelsLike : 0.0,
-    pressure : 0,
-    humidity : 0
-  );
+  late WeatherViewModel weatherViewModel;
 
   Future<Position> determinePosition() async {
     LocationPermission permission = await Geolocator.checkPermission();
@@ -45,24 +44,20 @@ class HomeViewState extends State<HomeView> {
     return await Geolocator.getCurrentPosition();
   }
 
-  void getCurrentLocation() async {
+  Future<void> getCurrentLocation() async {
     try {
       position = await determinePosition();
-      WeatherModel weatherModel =  await weatherViewModel.getWeather(position!.latitude, position!.longitude);
-      setState(() {
-        weatherData = weatherModel;
-      });
+      weatherViewModel = Provider.of<WeatherViewModel>(context, listen: false);
+      await weatherViewModel.getWeather(
+          position!.latitude, position!.longitude);
     } catch (e) {
       print(e);
     }
   }
 
-
   UserModel user = UserModel(uuid: 0, name: "Default", studentCode: 0);
 
   final UserModel _user = UserRepository.getState().state;
-
-  
 
   @override
   void initState() {
@@ -74,29 +69,52 @@ class HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: const BoxDecoration(
-            gradient: LinearGradient(
-                colors: [Color(0xFF16171B), Color(0xFF353A40)],
-                begin: Alignment.bottomRight,
-                end: Alignment.topLeft)),
-        child: Scaffold(
-          backgroundColor: Colors.transparent,
-          body: Padding(
-              padding: const EdgeInsets.only(
-                  top: 40, left: 20, right: 20, bottom: 20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Header(showUserInfo: true, user: user, showHomeIcon: false),
-                  const SizedBox(height: 20),
-                  const SizedBox(height: 20),
-                  Weather(weather: weatherData,),
-                  const SizedBox(height: 20),
-                  const University(),
-                  const SizedBox(height: 20),
-                  const Options(),
-                ],
-              )),
-        ));
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Color(0xFF16171B), Color(0xFF353A40)],
+          begin: Alignment.bottomRight,
+          end: Alignment.topLeft,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Padding(
+          padding: const EdgeInsets.only(
+            top: 40,
+            left: 20,
+            right: 20,
+            bottom: 20,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Header(
+                showUserInfo: true,
+                user: user,
+                showHomeIcon: false,
+              ),
+              const SizedBox(height: 20),
+              const SizedBox(height: 20),
+              Consumer<WeatherViewModel>(
+                builder: (context, weatherViewModel, _) {
+                  final weatherData = weatherViewModel.weatherData;
+                  if (weatherData != null) {
+                    return Weather(
+                      weather: weatherData,
+                    );
+                  } else {
+                    return CircularProgressIndicator();
+                  }
+                },
+              ),
+              const SizedBox(height: 20),
+              const University(),
+              const SizedBox(height: 10),
+              const Options(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
