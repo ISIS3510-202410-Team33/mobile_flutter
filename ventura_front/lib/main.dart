abd4851d27bf6c99e9ff40c14e5a0c2513b8eab8
Obed Cabanzo <78177466+ObedCabanzo@users.noreply.github.com>
2024-04-24 12:15:44 -0500

    Fixing errors in map screen - Language, distance and time attributes  #46

--- ventura_front/lib/main.dart
+++ ventura_front/lib/main.dart
@@ -10,7 +10,6 @@ Future<void> main() async {
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
-
   @override
   Widget build(BuildContext context) { 
     return const MaterialApp(
