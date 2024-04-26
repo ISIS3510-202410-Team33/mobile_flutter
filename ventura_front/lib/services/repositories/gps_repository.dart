2ba09f6bf7f268aa831a8e65859513c5b6cfa5fb
Obed Cabanzo <78177466+ObedCabanzo@users.noreply.github.com>
2024-04-26 00:57:06 -0500

    Implementation of internet connection check function

--- ventura_front/lib/services/repositories/gps_repository.dart
+++ ventura_front/lib/services/repositories/gps_repository.dart
@@ -8,6 +8,7 @@ final class GpsRepository extends SingletonBase<Position>{
 
   //Singleton Pattern
   static  GpsRepository? _instance;
+  
   GpsRepository._internal()  {
     Position positionInitial = Position(longitude: 0, latitude: 0, timestamp: DateTime.now(), accuracy: 0, altitude: 0, altitudeAccuracy: 0, heading: 0, headingAccuracy: 0, speed: 0, speedAccuracy: 0) ;
     state = positionInitial;
---
+++ ventura_front/lib/services/view_models/connection_viewmodel.dart
@@ -0,0 +1,92 @@
+import '../../mvvm_components/viewmodel.dart';
+import '../../mvvm_components/observer.dart';
+import 'dart:async';
+import 'dart:isolate';
+import 'dart:io';
+
+
+class ConnectionViewModel extends EventViewModel{
+
+  static final ConnectionViewModel _instance = ConnectionViewModel._internal();
+
+
+  late bool _isConnected;
+  late ReceivePort _receivePort;
+  late Isolate _isolate;
+  bool _started = false;
+
+  void spreadConnectionState(bool state){
+    notify(ConnectionEvent(connection: state));
+  }
+
+  factory ConnectionViewModel() {
+    return _instance;
+  }
+
+  ConnectionViewModel._internal() {
+    if (_started == false) {
+      _receivePort = ReceivePort();
+      _startInternetCheckIsolate();
+    }
+  }
+
+  void _startInternetCheckIsolate() async {
+    print("Creating isolate...");
+    _isolate = await  Isolate.spawn(_checkInternetIsolate, _receivePort.sendPort);
+    print("Isolate Created ");
+    // Escuchar el puerto para recibir mensajes del Isolate.
+    _receivePort.listen((dynamic data) {
+      
+      if (_started) {
+        if (_isConnected != data){
+          spreadConnectionState(data);
+        }
+      }
+      _isConnected = data;
+      _started = true;
+    });
+  }
+
+  // Método que se ejecutará en el Isolate.
+  static void _checkInternetIsolate(SendPort sendPort) {
+    Timer.periodic(const Duration(seconds: 5), (timer) async {
+      bool isConnected = await _checkInternetConnection();
+      sendPort.send(isConnected);
+    });
+  }
+
+  // Método para verificar la conexión a Internet.
+  static Future<bool> _checkInternetConnection() async {
+    try {
+      final result = await InternetAddress.lookup('google.com');
+      if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+        return true;
+      } else {
+        return false;
+      }
+    } on SocketException catch (_) {
+      return false;
+    }
+  }
+
+  bool isConnected() {
+    return _isConnected;
+  }
+
+  void disposeConnection() {
+    _receivePort.close();
+    _isolate.kill();
+  }
+
+  @override
+  void subscribe(EventObserver o) {
+    unsubscribeAll();
+    super.subscribe(o);
+  }
+
+}
+
+class ConnectionEvent extends ViewEvent {
+  bool connection;
+  ConnectionEvent({required this.connection}) : super("ConnectionEvent");
+}
