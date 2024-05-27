e52b4c0a211d72dd6ff427f732739e5c16578641
EvelinVillamil5 <e.villamilg@uniandes.edu.co>
2024-05-27 01:00:46 -0500

    logic courses implementation

---
+++ ventura_front/lib/services/models/course_model.dart
@@ -0,0 +1,28 @@
+class Course {
+  int id;
+  String name;
+  String description;
+  int date;
+  String professor;
+
+
+  Course({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.date,
+    required this.professor,
+  });
+
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'description': description,
+      'date': date,
+      'professor': professor,
+    };
+  }
+
+}
